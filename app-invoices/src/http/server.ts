2dd10e43c45c8c27c5cc61446e18a58075472705
AlexandreOliveira7 <serolfalexandre@gmail.com>
2025-06-05 16:47:19 -0300

    copiando e removendo o que não irá utilizar do orde para invoices

---
+++ app-invoices/src/http/server.ts
@@ -0,0 +1,31 @@
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { randomUUID } from 'node:crypto';
+import { z } from 'zod';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifyCors, {
+  origin: '*',
+});
+
+app.get('/health', () => {
+  return 'ok';
+});
+
+app
+  .listen({
+    host: '0.0.0.0',
+    port: 3333,
+  })
+  .then(() => {
+    console.log('[invoices] server running');
+  });
